439f1b2a74c00798321d8d7bbdbb2c56a89d2ffd
Brennan <brennan@CSdebian>
2015-10-11 10:58:54 -0400

    form css formatting

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = models.Comment
         fields = ('author', 'text')
+        labels = {'author': 'Name', 'text': 'Comment'}
+        widgets = {
+            'author': forms.TextInput(attrs={'required': ''}),
+            'text': forms.Textarea(attrs={'cols': 40, 'rows': 4,
+                                          'required': ''}),
+        }
 
