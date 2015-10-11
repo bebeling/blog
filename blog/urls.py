6724e81287c8e5321f8fad7e52d11a7af62142e5
Brennan <brennan@CSdebian>
2015-10-11 13:59:10 -0400

    cleanup

--- blog/urls.py
+++ blog/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     url(r'^archive/$', views.archive, name='archive'),
     url(r'^posts/$', views.post_list, name='post_list'),
     url(r'^posts/(?P<post_pk>[0-9]+)/$', views.blog_post, name='blog_post'),
-    url(r'^posts/(?P<post_pk>[0-9]+)/comment/$',
-        views.submit_comment,
-        name='comment',
-    ),
+    url(r'^posts/(?P<post_pk>[0-9]+)/comment/$', views.submit_comment,
+        name='comment'),
 ]
 
